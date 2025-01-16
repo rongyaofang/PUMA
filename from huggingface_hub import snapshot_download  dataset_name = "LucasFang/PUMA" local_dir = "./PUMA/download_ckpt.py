ddc94c9678c5bc283e59f11a2ff1dd6ed1528399
Chengqi Duan <43027865+gogoduan@users.noreply.github.com>
2025-01-16 14:09:52 +0800

    Create download_ckpt.py

---
+++ from huggingface_hub import snapshot_download  dataset_name = "LucasFang/PUMA" local_dir = "./PUMA/download_ckpt.py
@@ -0,0 +1,14 @@
+from huggingface_hub import snapshot_download
+
+dataset_name = "LucasFang/PUMA"
+local_dir = "./ckpts"
+token = "<token>"
+
+snapshot_download(
+    repo_id=dataset_name,
+    repo_type="model",
+    local_dir=local_dir,
+    token=token,
+    resume_download=True,
+    local_dir_use_symlinks=False,
+)
